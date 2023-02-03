20bc048165b63cdc78ea4b84eff36ac1f4b90989
Ron Barzilay <ron.barzilay@gmail.com>
2023-02-03 13:59:58 +0200

    Creating Components & Screens first ideas

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View, Pressable } from "react-native";
+
+// React Component
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default StartGameScreen;
